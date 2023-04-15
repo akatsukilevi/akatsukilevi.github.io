50b0d088b15b457446d73e6a34145d2f4a203c62
Akatsuki Levi <akatsukilevi@yahoo.co.jp>
2023-04-15 13:53:57 -0300

    Forgot Github build flag

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 
-const isGithub = process.env.ASTR_BUILD_GITHUB === 'true';
+const isGithub = process.env.ASTRO_BUILD_GITHUB === 'true';
 
 export default defineConfig({
 	publicDir: './assets',
